a7e5f468cabe4705ce2c07abeb3f583c95a6d564
IvanDBeltran <ivan.daniel.beltran@gmail.com>
2023-06-27 20:09:36 -0400

    Implemented object rotation and skybox

--- AdverturersQuest/Assets/Scripts/Rotate.cs
+++ AdverturersQuest/Assets/Scripts/Rotate.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,21 @@ public class Rotate : MonoBehaviour
     [SerializeField] private Transform m_Transform;
     [SerializeField] private float m_Speed;
 
+    private void Start()
+    {
+        CheckDependencies();
+    }
+
     void Update()
     {
         transform.Rotate( Vector3.forward, m_Speed * Time.deltaTime);
+    }
+
+    private void CheckDependencies()
+    {
+        if (!m_Transform)
+        {
+            m_Transform = GetComponent<Transform>();
+        }
     }
 }
